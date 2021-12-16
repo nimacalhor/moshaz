aee1d9c78b16210fa92d6e85ddb1356f0d999fd6
nimacalhor <nimakalhor25@gmail.com>
2021-12-16 15:56:16 +0330

    add footer and stuff

---
+++ script/script.js
@@ -0,0 +1,11 @@
+// const allSections = document.querySelectorAll("section");
+// const header = document.querySelector("header");
+// const footer= document.querySelector('footer');
+
+// allSections.forEach(function(el){
+//     if(el.classList.contains("dont-delete")) return
+//     el.remove()
+// })
+
+// header.remove()
+// footer.remove()
